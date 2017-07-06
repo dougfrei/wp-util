b368f974482a483a2bc967a1ed9d67fc391fce4f
Doug Frei <doug.frei@gravitatedesign.com>
2017-07-06 16:19:07 -0700

    added GravityForms functions to move JS to footer

--- src/Vendor/GravityForms.php
+++ src/Vendor/GravityForms.php
@@ -3,14 +3,15 @@ namespace WPUtil\Vendor;
 
 class GravityForms {
 	private static $_form_choices;
-	
+	private static $_move_scripts = array();
+
     public static function get_all_forms() {
 		if (!self::$_form_choices) {
 			$form_choices = array();
-			
-            if (method_exists('RGFormsModel','get_forms')) {
+
+            if (method_exists('RGFormsModel', 'get_forms')) {
 			    $forms = \RGFormsModel::get_forms(null, 'title');
-			    
+
                 foreach($forms as $form) {
 			        $form_choices[$form->id] = $form->title;
 			    }
@@ -18,7 +19,35 @@ class GravityForms {
 
 			self::$_form_choices = $form_choices;
 		}
-        
+
 		return self::$_form_choices;
+	}
+
+	public static function move_scripts_to_footer() {
+		// add_filter('gform_init_scripts_footer', '__return_true');
+		add_filter('gform_get_form_filter', array('Grav\GravityForms', '_move_scripts_form_filter'), 10, 2);
+		add_action('wp_footer', array('Grav\GravityForms', '_move_scripts_footer_print'), 999);
+	}
+
+	public static function _move_scripts_form_filter($form_string, $form) {
+		$matches = array();
+
+		preg_match_all("/<script\b[^>]*>([\s\S]*?)<\/script>/", $form_string, $matches);
+
+		if (isset($matches[0]) && is_array($matches[0])) {
+			self::$_move_scripts = array_merge(self::$_move_scripts, $matches[0]);
+
+			return preg_replace("/<script\b[^>]*>([\s\S]*?)<\/script>/", '', $form_string);
+		}
+
+		return $form_string;
+	}
+
+	public static function _move_scripts_footer_print() {
+		$scripts = array_unique(self::$_move_scripts);
+
+		foreach ($scripts as $script) {
+			echo $script."\n";
+		}
 	}
 }
