f71a249190a96beef4c1d90d1574754221e02782
Doug Frei <dougfrei@gmail.com>
2021-09-12 20:10:25 -0700

    Allow functions to be used for returning default values in Arrays::get_value_as_*

--- src/Arrays.php
+++ src/Arrays.php
@@ -8,17 +8,17 @@ abstract class Arrays
 	 *
 	 * @param array $values
 	 * @param string $key
-	 * @param string $default
+	 * @param string|callable $default
 	 * @param boolean $trim
 	 * @return string
 	 */
-	public static function get_value_as_string(array $values, string $key, string $default = '', bool $trim = true): string
+	public static function get_value_as_string(array $values, string $key, $default = '', bool $trim = true): string
 	{
 		if (isset($values[$key]) && is_string($values[$key])) {
 			return $trim ? trim($values[$key]) : $values[$key];
 		}
 
-		return $default;
+		return is_callable($default) ? $default() : $default;
 	}
 
 	/**
@@ -26,12 +26,12 @@ abstract class Arrays
 	 *
 	 * @param array $values
 	 * @param string $key
-	 * @param array $default
+	 * @param array|callable $default
 	 * @return array
 	 */
-	public static function get_value_as_array(array $values, string $key, array $default = []): array
+	public static function get_value_as_array(array $values, string $key, $default = []): array
 	{
-		return (isset($values[$key]) && is_array($values[$key])) ? $values[$key] : $default;
+		return (isset($values[$key]) && is_array($values[$key])) ? $values[$key] : (is_callable($default) ? $default() : $default);
 	}
 
 	/**
@@ -39,13 +39,13 @@ abstract class Arrays
 	 *
 	 * @param array $values
 	 * @param string $key
-	 * @param boolean $default
+	 * @param boolean|callable $default
 	 * @return boolean
 	 */
-	public static function get_value_as_bool(array $values, string $key, bool $default = false): bool
+	public static function get_value_as_bool(array $values, string $key, $default = false): bool
 	{
 		if (!isset($values[$key])) {
-			return $default;
+			return is_callable($default) ? $default() : $default;
 		}
 
 		return is_bool($values[$key]) ? $values[$key] : intval($values[$key]) === 1;
@@ -56,12 +56,12 @@ abstract class Arrays
 	 *
 	 * @param array $values
 	 * @param string $key
-	 * @param integer $default
+	 * @param integer|callable $default
 	 * @return integer
 	 */
-	public static function get_value_as_int(array $values, string $key, int $default = 0): int
+	public static function get_value_as_int(array $values, string $key, $default = 0): int
 	{
-		return isset($values[$key]) ? intval($values[$key]) : $default;
+		return isset($values[$key]) ? intval($values[$key]) : (is_callable($default) ? $default() : $default);
 	}
 
 	/**
